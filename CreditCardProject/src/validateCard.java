e4ce623a53419002e7d4d16bd2c0e554fb927acf
sally.kohara <sally.kohara@MU-C1-11.mullen.pvt.k12.co.us>
2025-04-07 10:34:11 -0600

    first commit

---
+++ CreditCardProject/src/validateCard.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner; 
+
+public class validateCard
+	{
+
+		public static <textFile> void main(int[] args) throws IOException
+			{
+				ArrayList<textFile> Number = new ArrayList<>();
+				getCreditCardNumber();
+				luhnsMethod();
+
+			}
+		private static void getCreditCardNumber()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+		private static void luhnsMethod()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+		public static void getTextFile() throws IOException
+			{
+				Scanner file = new Scanner(new File("CreditCardNumber.txt"));
+				while(file.hasNext())
+					{
+						String Number = file.next();
+					}
+
+		
+	}
