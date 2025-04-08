33d7efcf108319eb99c8e85bc06c7c659bf3a7cd
sally.kohara <sally.kohara@MU-C1-11.mullen.pvt.k12.co.us>
2025-04-08 12:51:56 -0600

    trynmg

---
+++ CreditCardProject/src/textFile.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner; 
+
+public class textFile
+	{
+		
+		public static void main(String[] args) throws IOException
+			{
+				ArrayList<textFile> Student = new ArrayList<>();
+				getTextFile();
+			}
+			
+		public static void getTextFile() throws IOException
+		{
+			Scanner file = new Scanner(new File("newTextFile.txt"));
+			while(file.hasNext())
+				{
+					String firstName = file.next();
+					Student.add(new Student(String f, String l))
+				    System.out.println(firstName);
+				}
+		}
+
+	}//meow!!!!
